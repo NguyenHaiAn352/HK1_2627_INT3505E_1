7e14c5695ba1b45fbed28c47057ded70be897a67
NguyenHaiAn352 <24021352@vnu.edu.vn>
2026-09-10 22:22:26 +0700

    Week 1 - Problem 1 + 2
    took notes on what were confusing on the codes

--- Week 1/app1.py
+++ Week 1/app1.py
@@ -6,8 +6,9 @@ def connect():
     return jsonify({"status":"ok"}), 200
 # Return to the client what the client sent - echoing
 @app.route("/echo", methods=["POST"])
+# Single quote the data, escape double quote to treat them as characters within the data
 def echo():
     data = request.get_json(silent=True) or {}
-    return jsonify({"What were sent: ": data}), 200
+    return jsonify({"What were sent": data}), 200
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port= 5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
